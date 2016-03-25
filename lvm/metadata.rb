6472b7dff576f112778e3d81ff0a522824297e2d
Benjamin Davison <ben@ngsc.io>
2016-03-25 10:49:26 +0000

    Initial commit

---
+++ lvm/metadata.rb
@@ -0,0 +1,16 @@
+name 'lvm'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache 2.0'
+description 'Installs and manages Logical Volume Manager'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '1.6.1'
+
+%w(amazon centos fedora freebsd oracle redhat scientific sles ubuntu).each do |os|
+  supports os
+end
+
+recipe 'lvm', 'Installs lvm2 package'
+
+source_url 'https://github.com/chef-cookbooks/lvm' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/lvm/issues' if respond_to?(:issues_url)
---
+++ lvm/test/fixtures/cookbooks/test/metadata.rb
@@ -0,0 +1,8 @@
+name 'test'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache 2.0'
+description 'A test cookbook for lvm'
+version '0.1.0'
+
+depends 'lvm'
